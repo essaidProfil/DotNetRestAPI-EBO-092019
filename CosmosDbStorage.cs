70adc518dcc5428321c306b776a966a56aa785f7
essaidProfil <essaid.profil@gmail.com>
2019-10-02 00:50:46 +0100

    02102019 - Creating Order Model

--- CosmosDbStorage.cs
+++ CosmosDbStorage.cs
@@ -11,12 +11,21 @@ using System.Web;
 
 namespace Catalogue
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public static class CosmosDbStorage<T> where T : class
     {
         private static readonly string DatabaseId = ConfigurationManager.AppSettings["database"];
         private static readonly string CollectionId = ConfigurationManager.AppSettings["collection"];
         private static DocumentClient client;
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="category"></param>
+        /// <returns></returns>
         public static async Task<T> GetItemAsync(string id, string category)
         {
             try
@@ -37,7 +46,11 @@ namespace Catalogue
                 }
             }
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="predicate"></param>
+        /// <returns></returns>
         public static async Task<IEnumerable<T>> GetItemsAsync(Expression<Func<T, bool>> predicate)
         {
             IDocumentQuery<T> query = client.CreateDocumentQuery<T>(
@@ -54,41 +67,66 @@ namespace Catalogue
 
             return results;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public static async Task<Document> CreateItemAsync(T item)
         {
             return await client.CreateDocumentAsync(UriFactory.CreateDocumentCollectionUri(DatabaseId, CollectionId), item);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
         public static async Task<Document> UpdateItemAsync(string id, T item)
         {
             return await client.ReplaceDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id), item);
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="category"></param>
+        /// <returns></returns>
         public static async Task DeleteItemAsync(string id, string category)
         {
             await client.DeleteDocumentAsync(UriFactory.CreateDocumentUri(DatabaseId, CollectionId, id), new RequestOptions { PartitionKey = new PartitionKey(category) });
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
         public static void Initialize()
         {
             client = new DocumentClient(new Uri(ConfigurationManager.AppSettings["endpoint"]), ConfigurationManager.AppSettings["authKey"]);
             CreateDatabaseIfNotExistsAsync().Wait();
             CreateCollectionIfNotExistsAsync("/category").Wait();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="authKey"></param>
         public static void Initialize(string endpoint, string authKey)
         {
             client = new DocumentClient(new Uri(endpoint), authKey);
             CreateDatabaseIfNotExistsAsync().Wait();
             CreateCollectionIfNotExistsAsync("/category").Wait();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
         public static void Teardown()
         {
             DeleteDatabaseAsync().Wait();
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private static async Task CreateDatabaseIfNotExistsAsync()
         {
             try
@@ -107,7 +145,11 @@ namespace Catalogue
                 }
             }
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="partitionkey"></param>
+        /// <returns></returns>
         private static async Task CreateCollectionIfNotExistsAsync(string partitionkey)
         {
             try
@@ -136,7 +178,10 @@ namespace Catalogue
                 }
             }
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         private static async Task DeleteDatabaseAsync()
         {
             await client.DeleteDatabaseAsync((UriFactory.CreateDatabaseUri(DatabaseId)));
---
+++ Models/Order.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Catalogue.Models
+{
+    public class Order
+    {
+    }
+}
