a8bba2cfbeca38879d3af4dfefd38f3784b40861
essaidProfil <essaid.profil@gmail.com>
2019-10-09 23:24:43 +0100

    09102019 - Plusieurs modification sur Order Model & Order Controller

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -12,26 +12,28 @@ using Newtonsoft.Json;
 namespace Catalogue.Controllers
 {
     /// <summary>
-    /// 
+    /// Order Controller
     /// </summary>
     public class OrderController : ApiController
     {
         /// <summary>
-        /// Get all orders
+        /// Place Order
         /// </summary>
+        /// <param name="order"></param>
         /// <returns></returns>
-        [HttpGet]
-        [ActionName("GetOrders")]
-        public async Task<HttpResponseMessage> GetOrders()
+        [HttpPost]
+        [ActionName("AddOrder")]
+        public async Task<HttpResponseMessage> AddOrder([System.Web.Mvc.Bind(Include = "Id,Fname,Lname,Address,Town,Country,Email,Phone,ProductId,Quantity,DateOrder,Status,TotalPrice")] Order order)
         {
             try
             {
-                var result = await CosmosDbStorage<Order>.GetItemsAsync(o => o.UserID != null); ;
-                return new HttpResponseMessage
+                if (ModelState.IsValid)
                 {
-                    StatusCode = HttpStatusCode.Found,
-                    Content = new StringContent(JsonConvert.SerializeObject(result), Encoding.UTF8, "application/json")
-                };
+                    var result = await CosmosDbStorage<Order>.CreateItemAsync(order);
+
+                    return new HttpResponseMessage(HttpStatusCode.Created);
+                }
+                return new HttpResponseMessage(HttpStatusCode.BadRequest);
             }
             catch (Exception ex)
             {
@@ -39,23 +41,45 @@ namespace Catalogue.Controllers
             }
         }
         /// <summary>
-        /// Place Order
+        /// Enlever un produit du panier
         /// </summary>
-        /// <param name="Order"></param>
+        /// <param name="id"></param>
         /// <returns></returns>
-        [HttpPost]
-        [ActionName("AddOrder")]
-        public async Task<HttpResponseMessage> AddOrder([System.Web.Mvc.Bind(Include = "Id,ProductId,Quantity,UserID,DateOrder,Status,TotalPrice")] Order Order)
+        [HttpDelete]
+        [ActionName("DeleteOrder/{id}")]
+        public async Task<HttpResponseMessage> DeleteOrder(string id)
         {
             try
             {
-                if (ModelState.IsValid)
-                {
-                    var result = await CosmosDbStorage<Order>.CreateItemAsync(Order);
+                if (string.IsNullOrEmpty(id))
+                    return new HttpResponseMessage(HttpStatusCode.BadRequest);
 
-                    return new HttpResponseMessage(HttpStatusCode.Created);
-                }
-                return new HttpResponseMessage(HttpStatusCode.BadRequest);
+                await CosmosDbStorage<Order>.DeleteAsync(id);
+
+                return new HttpResponseMessage(HttpStatusCode.NoContent);
+            }
+            catch (Exception ex)
+            {
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex.Message));
+            }
+        }
+
+        /// <summary>
+        /// Get all orders
+        /// </summary>
+        /// <returns></returns>
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.ActionName("GetOrders")]
+        public async Task<HttpResponseMessage> GetOrders()
+        {
+            try
+            {
+                var result = await CosmosDbStorage<Order>.GetItemsAsync(o => o.Id != null); ;
+                return new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.Found,
+                    Content = new StringContent(JsonConvert.SerializeObject(result), Encoding.UTF8, "application/json")
+                };
             }
             catch (Exception ex)
             {
--- Models/Order.cs
+++ Models/Order.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -11,12 +13,76 @@ namespace Catalogue.Models
     /// </summary>
     public class Order
     {
+
         /// <summary>
-        /// Item id
+        /// 
         /// </summary>
         [JsonProperty(PropertyName = "id")]
-        public string Id { get; set; }
+        public int Id { get; set; }
         
+        /// <summary>
+        /// Champ du Prénom 
+        /// </summary>
+        [JsonProperty(PropertyName = "firstname")]
+        [Required(ErrorMessage = "Le Prénom est obligatoire !")]
+        [DisplayName("Prénom")]
+        [StringLength(160)]
+        public string Fname { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [JsonProperty(PropertyName = "lastname")]
+        [Required(ErrorMessage = "Le Nom est obligatoire !")]
+        [DisplayName("Nom")]
+        [StringLength(160)]
+        public string Lname { get; set; }
+
+        /// <summary>
+        /// Champ d'Adresse
+        /// </summary>
+        [JsonProperty(PropertyName = "adresse")]
+        [Required(ErrorMessage = "l'adresse est obligatoire !")]
+        [DisplayName("Adresse")]
+        [StringLength(70)]
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Town
+        /// </summary>
+        [JsonProperty(PropertyName = "ville")]
+        [Required(ErrorMessage = "Le nom de la ville est obligatoire !")]
+        [DisplayName("Ville")]
+        [StringLength(40)]
+        public string Town { get; set; }
+
+        /// <summary>
+        /// Champ du pays
+        /// </summary>
+        [JsonProperty(PropertyName = "pays")]
+        [Required(ErrorMessage = "Le nom de votre pays est obligatoire !")]
+        [DisplayName("Votre pays")]
+        [StringLength(40)]
+        public string Country { get; set; }
+
+        /// <summary>
+        /// Email
+        /// </summary>
+        [JsonProperty(PropertyName = "courriel")]
+        [Required(ErrorMessage = "Vous devez renseigner votre adresse couriel")]
+        [DisplayName("Votre courriel")]
+        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}",
+            ErrorMessage = "L'adresse courriel est invalid.")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Numéro de téléphone
+        /// </summary>
+        [JsonProperty(PropertyName = "tel")]
+        [StringLength(25)]
+        public string Phone { get; set; }
+
         /// <summary>
         /// Product ordered id
         /// </summary>
@@ -28,12 +94,6 @@ namespace Catalogue.Models
         /// </summary>
         [JsonProperty(PropertyName = "qty")]
         public string Quantity { get; set; }
-
-        /// <summary>
-        /// Custumer ID - Client
-        /// </summary>
-        [JsonProperty(PropertyName = "customer_id")]
-        public string UserID { get; set; }
 
         /// <summary>
         /// Order date
