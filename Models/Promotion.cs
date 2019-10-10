3e72e08da33b86d26044abbfc8866c461f8dfca7
essaidProfil <essaid.profil@gmail.com>
2019-10-10 19:51:50 +0100

    10102019 - Ajout du Model Promotion

---
+++ Models/Promotion.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Catalogue.Models
+{
+    /// <summary>
+    /// First promotion type
+    /// </summary>
+    public class Promotion
+    {
+        /// <summary>
+        /// Promotion Id
+        /// </summary>
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+        /// <summary>
+        /// Promotion Name
+        /// </summary>
+        [JsonProperty(PropertyName = "promotion_name")]
+        public string PromotionName { get; set; }
+
+        /// <summary>
+        /// Promotion start date 
+        /// </summary>
+        [JsonProperty(PropertyName = "start_date")]
+        public string StartPromo { get; set; }
+
+        /// <summary>
+        /// Promotion End date
+        /// </summary>
+        [JsonProperty(PropertyName = "end_date")]
+        public string EndPromo { get; set; }
+
+
+
+    }
+}
