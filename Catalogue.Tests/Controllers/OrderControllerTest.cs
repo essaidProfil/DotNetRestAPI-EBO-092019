a39001bc664ad093fed19110755ec867b5a71490
essaidProfil <essaid.profil@gmail.com>
2019-10-11 03:12:44 +0100

    11102019 - Chargement du projet Tests

---
+++ Catalogue.Tests/Controllers/OrderControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Catalogue.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catalogue.Tests.Controllers
+{
+    [TestClass]
+    public class OrderControllerTest
+    {
+        private string id;
+        private Order order;
+
+        /// <summary>
+        /// Get all order Tests
+        /// </summary>
+        [TestMethod]
+        public void Get_ALL_Orders()
+        {
+            OrderControllerTest controller = new OrderControllerTest();
+
+            var results = CosmosDbStorage<Order>.GetItemsAsync(o => o.Id != null);
+
+            Assert.IsNotNull(results);
+        }
+
+        /// <summary>
+        /// Test Methode - Ajouter un produit au panier
+        /// </summary>
+        [TestMethod]
+        public void Place_Order() {
+            OrderControllerTest Controller = new OrderControllerTest();
+
+            var results = CosmosDbStorage<Order>.CreateItemAsync(order);
+
+            Assert.IsNotNull(results);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        [TestMethod]
+        public void DeleteOrder() {
+
+            OrderControllerTest Controller = new OrderControllerTest();
+            var result = CosmosDbStorage<Order>.DeleteAsync(id);
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
---
+++ Catalogue.Tests/Controllers/ProductControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Catalogue.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catalogue.Tests.Controllers
+{
+    [TestClass]
+    public class ProductController
+    {
+        [TestMethod]
+        public void GetItemsAsync()
+        {
+            // Arrange
+            ProductController controller = new ProductController();
+
+            // Act
+            var results = CosmosDbStorage<Product>.GetItemsAsync(p => p.InStock);
+            // Assert
+            Assert.IsNotNull(results);
+        }
+        [TestMethod]
+        public void GetProduct_byId() {
+
+            ProductController controller = new ProductController();
+
+            var result = CosmosDbStorage<Product>.GetItemsAsync(d => d.InStock == true);
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
